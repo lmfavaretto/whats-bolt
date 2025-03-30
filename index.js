e2a4dc084c0ba3bca4ae69dea18b046af4a4673f
lmfavaretto <55554697+lmfavaretto@users.noreply.github.com>
2025-03-30 19:06:23 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
 const express = require('express');
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode');
 const app = express();
-const port = process.env.PORT || 8080;
+const { default: puppeteer } = require('puppeteer');
 
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    }
+app.get('/', async (req, res) => {
+  res.send('Servidor rodando!');
 });
 
-let qrCodeData = '';
-
-client.on('qr', (qr) => {
-    qrcode.toDataURL(qr, (err, url) => {
-        qrCodeData = url;
-    });
-});
-
-client.on('ready', () => {
-    console.log('✅ Cliente está pronto!');
-});
-
-client.initialize();
-
-app.get('/qr', (req, res) => {
-    if (qrCodeData) {
-        res.send(`<img src="${qrCodeData}" />`);
-    } else {
-        res.send('QR Code não gerado ainda...');
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+app.listen(8080, () => {
+  console.log('Servidor rodando na porta 8080');
 });
